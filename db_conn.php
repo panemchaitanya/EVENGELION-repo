0692d67539000764c3c78bc8c1e52f038502e905
Panem Chaitanya Pavan Kumar <chintuchaitanya2@gmail.com>
2024-01-05 18:18:22 +0530

    Add files via upload

---
+++ db_conn.php
@@ -0,0 +1,14 @@
+<?php 
+
+$sName = "localhost";
+$uName = "root";
+$pass = "";
+$db_name = "data_base_name";
+
+try {
+    $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
+                    $uName, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+  echo "Connection failed : ". $e->getMessage();
+}
